4806ff9d788ca78bd207f6e04c770c7b121e2fe8
andrea <andrea.bortolossi89@gmail.com>
2025-09-23 12:31:48 +0200

    automate proxy conf

--- scripts/start_proxy.sh
+++ scripts/start_proxy.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Path template nel repo
+TEMPLATE="/var/www/html/proxy/proxy.conf.template"
+DEST="/etc/httpd/conf.d/proxy.conf"
+# Recupera IP pubblico della EC2
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+EC2_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/public-ipv4)
+
+# Sostituisci il placeholder e salva in /etc/httpd/conf.d
+sed "s/{{EC2_IP}}/$EC2_IP/" $TEMPLATE > $DEST
+# Controlla sintassi
+apachectl configtest
+# Riavvia Apache
+systemctl restart httpd
+
 mkdir -p /home/ec2-user/proxy
 cp -r /var/www/html/proxy/* /home/ec2-user/proxy/
 chown -R ec2-user:ec2-user /home/ec2-user/proxy
