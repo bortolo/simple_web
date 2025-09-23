ab860d502769b295ba462daf65b30c047f16910b
andrea <andrea.bortolossi89@gmail.com>
2025-09-23 10:09:07 +0200

    npm dependencies

--- scripts/start_proxy.sh
+++ scripts/start_proxy.sh
@@ -4,4 +4,5 @@ cp -r /var/www/html/proxy/* /home/ec2-user/proxy/
 chown -R ec2-user:ec2-user /home/ec2-user/proxy
 chmod -R 755 /home/ec2-user/proxy
 cd /home/ec2-user/proxy
-nohup node server.js > proxy.log 2>&1 &
+npm install
+nohup node proxy.js > proxy.log 2>&1 &
