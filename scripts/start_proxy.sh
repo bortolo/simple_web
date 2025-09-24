13629b6c3952372e524304a5ec261339ab3272a1
andrea <andrea.bortolossi89@gmail.com>
2025-09-24 09:56:17 +0200

    fix permissions proxy

--- scripts/start_proxy.sh
+++ scripts/start_proxy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-echo "=== 🚀 Avvio proxy Node.js ==="
-chown -R ec2-user:ec2-user /home/ec2-user/proxy
+echo "=== 🚀 Avvio script proxy Node.js ==="
+# Devo elevarmi a root per cambiare anche la proprietà di proxy.js
+sudo chown -R ec2-user:ec2-user /home/ec2-user/proxy
 chmod -R 755 /home/ec2-user/proxy
 cd /home/ec2-user/proxy
+echo "--- Install dipendenze ---"
 npm init -y
 npm install express node-fetch@2 aws-sdk
+echo "--- Avvio processo ---"
 nohup node proxy.js > proxy.log 2>&1 &
