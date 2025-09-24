98b8f288ef7ad3c864b172e83c0dd252c70309f6
andrea <andrea.bortolossi89@gmail.com>
2025-09-24 16:15:17 +0200

    deciamal layout

--- webserver/js/ui.js
+++ webserver/js/ui.js
@@ -5,9 +5,9 @@
     const tr = document.createElement("tr");
     tr.innerHTML = `
       <td class="border border-gray-300 p-2">Anno ${i}</td>
-      <td class="border border-gray-300 p-2"><input type="number" name="rev_${i}" class="w-full border rounded p-1"></td>
-      <td class="border border-gray-300 p-2"><input type="number" name="ebitda_${i}" class="w-full border rounded p-1"></td>
-      <td class="border border-gray-300 p-2"><input type="number" name="cpx_${i}" class="w-full border rounded p-1"></td>
+      <td class="border border-gray-300 p-2"><input type="number" name="rev_${i}" step="0.01" min="0" class="w-full border rounded p-1"></td>
+      <td class="border border-gray-300 p-2"><input type="number" name="ebitda_${i}" step="0.01" min="0" max="1" class="w-full border rounded p-1"></td>
+      <td class="border border-gray-300 p-2"><input type="number" name="cpx_${i}" step="0.01" min="0" class="w-full border rounded p-1"></td>
     `;
     tbody.appendChild(tr);
   }
