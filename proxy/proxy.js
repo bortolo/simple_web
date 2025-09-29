0c6b5d9e226be2b893a08c88c5128f33e2a60753
andrea <andrea.bortolossi89@gmail.com>
2025-09-29 11:39:21 +0200

    new name api

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -25,7 +25,7 @@ async function getRestApiId(apiName) {
 
 // Funzione per costruire URL completo
 async function getLambdaUrl() {
-  const apiId = await getRestApiId("my-private-api");
+  const apiId = await getRestApiId("my_private_api");
   const stage = "prod";
   const region = "eu-central-1";
   return `https://${apiId}.execute-api.${region}.amazonaws.com/${stage}`;
